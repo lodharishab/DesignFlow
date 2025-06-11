c3a2b4de96e06c7294e723b85b6ac5e179181b72
Maitreya Patni <maitreyapatni@gmail.com>
2025-06-11 16:11:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from '@/lib/utils';
 import { UIProvider } from '@/contexts/ui-context';
 import { MobileBottomNav } from '@/components/layout/mobile-bottom-nav';
 import Script from 'next/script';
+import { ClientOnly } from '@/components/shared/client-only';
 
 const oswald = Oswald({
   subsets: ['latin'],
@@ -36,7 +37,11 @@ export default function RootLayout({
         <Script src="https://checkout.razorpay.com/v1/checkout.js" strategy="lazyOnload" />
       </head>
       <body className="antialiased min-h-screen flex flex-col pb-16 md:pb-0">
-        <UIProvider>{children}<MobileBottomNav /></UIProvider><Toaster />
+        <UIProvider>
+          {children}
+          <ClientOnly><MobileBottomNav /></ClientOnly>
+        </UIProvider>
+        <ClientOnly><Toaster /></ClientOnly>
       </body>
     </html>
   );
